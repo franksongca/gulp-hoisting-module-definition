cbcc50bddbf329aded0f255c84456e6f1ebdc54e
Frank Song <frank.song@mobilelive.ca>
2016-02-16 13:00:16 -0500

    refactor.

--- index.js
+++ index.js
@@ -1,85 +1,59 @@
 'use strict';
 
-var BufferStreams = require("bufferstreams"),
-  through = require('through2'),
+var through = require('through2'),
   _ = require('underscore-node');
 
-function transform(input) {
+function hoist(input) {
   var res = input,
     pieces = res.split('(function () {'),
     defPiece,
     defIndex,
     buffer;
 
-    defPiece =_.find(pieces, function (piece, index) {
-      if (piece.indexOf('angular.module(\'ute.ui\', [') >= 0) {
-        defIndex = index;
-        return true;
-      };
-    });
+  defPiece =_.find(pieces, function (piece, index) {
+    if (piece.indexOf('angular.module(\'ute.ui\', [') >= 0) {
+      defIndex = index;
+      return true;
+    };
+  });
 
-    if (defIndex !== 1) {
-      console.log('ute-ui module definition is not on the most top, correct it!');
-      // if the module definition is not on the top
-      pieces[defIndex] = pieces[1];
-      pieces[1] = defPiece;
-    }
-
+  if (defIndex !== 1) {
+    console.log('ute-ui module definition is not on the most top, correct it!');
+    // if the module definition is not on the top
+    pieces[defIndex] = pieces[1];
+    pieces[1] = defPiece;
+  }
 
   res = pieces.join('(function () {');
 
-  var buffer = new Buffer(res.length);
+  buffer = new Buffer(res.length);
 
   buffer.write(res, "utf-8");
 
   return buffer;
 }
 
-module.exports = function(stream) {
+module.exports = function() {
   console.error('hoisting module definition...');
 
   /**
    * buffer each content
-   * @param file
+   * @param stream
    * @param enc
    * @param callback
    */
-  var bufferedContents = function (file, enc, callback) {
+  var bufferedContents = function (stream, enc, callback) {
 
-    if (file.isBuffer()) {
+    if (stream.isBuffer()) {
       //console.log(file.contents.toString());
-      file.contents = transform(file.contents.toString());
+      stream.contents = hoist(stream.contents.toString());
     }
-
-    //if (file.isStream()) {
-    //
-    //  this.emit('error', new gutil.PluginError('gulp-ng-module-renamer', 'Streams are not supported!'));
-    //  callback();
-    //
-    //} else if (file.isNull()) {
-    //
-    //  callback(null, file); // Do nothing if no contents
-    //
-    //} else {
-    //
-    //  var ctx = file.contents.toString('utf8'),
-    //    modulesString = reNameModules(ctx);
-    //
-    //  file.contents = new Buffer(modulesString);
-    //  callback(null, file);
-    //
-    //}
-
-    //this.push(file);
-    //done();
-
-    callback(null, file); // Do nothing if no contents
+    callback(null, stream);
   };
 
   /**
    * returns streamed content
    */
   return through.obj(bufferedContents);
-
 
 };
